8cfbd2b56f63644610b28be40adb97146373a5b1
abdullahshaik697 <abdullahshaik55555@gmail.com7>
2025-04-08 02:40:16 +0500

    first commit

---
+++ README.md
@@ -0,0 +1,28 @@
+This Project is a Todo App connected with a local mongoDB Database 
+
+
+Code Structure->
+controllers            
+    |---index.js                It has the main logics (functions) that will be used.
+
+model                           Schema/Structur of a single todo.
+    |---index.js
+
+routes                          Controls the routing, On which method and 
+    |---index.js                on which endpoint the api is triggered.
+
+middlewares                     Have the middleware logic that stores the Logs information.    
+    |---index.js                
+                  
+connection.js                   Store the mongodb connection logic
+
+log.txt                         Store the logs (or history)
+
+index.js                        Main execution file
+
+views
+    empty    
+    
+
+
+DB Structure->
---
+++ index.js
@@ -0,0 +1,72 @@
+const express = require(`express`)
+const mongoose = require(`mongoose`)
+const app = express()
+
+const PORT = 3000
+
+mongoose.connect("mongodb+srv://root:root123@mongodbproject1.4stku.mongodb.net/Todo_App?retryWrites=true&w=majority")
+    .then(() => console.log("Connected to DB"))
+    .catch(err => console.error("DB Error:", err));
+
+const todoSchema = new mongoose.Schema({
+    task: {
+        type: String,
+        required: true
+    }
+
+}, { timestamps: true }
+)
+
+const Todo = mongoose.model(`todo`, todoSchema)
+
+app.use(express.urlencoded({ extended: false }))
+
+app.post(`/api/todos`, async (req, res) => {
+
+    const title = req.body.title;
+
+    try {
+        const newTask = new Todo({ task: title }); // create a Todo instance
+        await newTask.save(); //  save to DB
+        res.status(201).send("Task Added");
+    } catch (err) {
+        console.error("Error saving task:", err);
+        res.status(500).send("Error adding task");
+    }
+
+})
+app.get(`/api/todos`, async (req, res) => {
+
+    const allDbTodos = await Todo.find({})
+
+    const html = `
+        <h1>TODOS</h1>
+        <h3>${allDbTodos.map((todo) => `<li>${todo.task}</li>`).join("")}</h3>    
+        `
+    res.send(html)
+})
+app.get(`/api/todos/:id`, async (req, res) => {
+
+    const singleTodo = await Todo.findById(req.params.id)
+
+    const html = `
+        <h1>TODO</h1>
+        <h3>${singleTodo.task}</h3>    
+        `
+    res.send(html)
+})
+// app.delete(`/api/todos`, (req, res) => {
+
+// })
+
+app.patch(`/api/todos/:id`,async (req, res)=>{
+  await Todo.findByIdAndUpdate(req.params.id, {task: "texst update"})
+    res.json({"mesage": "success"})
+})
+
+
+app.listen(PORT, () => {
+    console.log("SERVER STARTED");
+
+})
+
